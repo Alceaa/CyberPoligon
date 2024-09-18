468ce9f017a0426343ffc39f471bca4a7b0a3dca
QQeWi <113581436+IuriKiriak@users.noreply.github.com>
2024-09-18 20:54:17 +0300

    Add files via upload
    API

--- cyberpolygon/cyberpolygonApp/views.py
+++ cyberpolygon/cyberpolygonApp/views.py
@@ -1,6 +1,65 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework import generics
+from .serializers import *
+
+
+class UserListCreate(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+
+class RoleListCreate(generics.ListCreateAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+class RoleRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Role.objects.all()
+    serializer_class = RoleSerializer
+
+
+class CategoryListCreate(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class CategoryRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class TaskListCreate(generics.ListCreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class TaskRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class CommentsListCreate(generics.ListCreateAPIView):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
+class CommentsRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comments.objects.all()
+    serializer_class = CommentsSerializer
+
+
+class UserAvatarListCreate(generics.ListCreateAPIView):
+    queryset = UserAvatar.objects.all()
+    serializer_class = UserAvatarSerializer
+
+class UserAvatarRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserAvatar.objects.all()
+    serializer_class = UserAvatarSerializer
+
+
 def init(request):
     return render(
             request,
